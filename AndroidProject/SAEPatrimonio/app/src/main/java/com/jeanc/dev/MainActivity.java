97acf2d92416e867beec8f437dae993a25e70999
jeancsilva20 <jeancsilva.20@gmail.com>
2018-01-24 22:58:52 -0200

    Teste leitor código

--- AndroidProject/SAEPatrimonio/app/src/main/java/com/jeanc/dev/MainActivity.java
+++ AndroidProject/SAEPatrimonio/app/src/main/java/com/jeanc/dev/MainActivity.java
@@ -9,17 +9,34 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.ImageView;
+
+import com.google.android.gms.vision.barcode.Barcode;
+import com.google.android.gms.vision.barcode.BarcodeDetector;
+import com.google.zxing.integration.android.IntentIntegrator;
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
 
+    private ImageView leitura;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON,WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
 
+        //cria o botão para a tela do scanner
+        leitura = (ImageView)findViewById(R.id.leitura);
+        leitura.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                abrirLeitor();
+            }
+        });
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
                 this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
@@ -28,7 +45,17 @@ public class MainActivity extends AppCompatActivity
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
+
     }
+    //chama a intent para leitura de código de barras
+    public void abrirLeitor(){
+        new IntentIntegrator(this).initiateScan();
+    }
+
+
+
+
+
 
     @Override
     public void onBackPressed() {
